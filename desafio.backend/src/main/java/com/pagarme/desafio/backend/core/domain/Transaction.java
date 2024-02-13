5f54b473f3246a2159ed96963060295afdd7fcdc
Salomao <eduardo.salomao.584@gmail.com>
2024-02-12 22:04:29 -0300

    Alterando tipo do ExpirationDate

--- desafio.backend/src/main/java/com/pagarme/desafio/backend/core/domain/Transaction.java
+++ desafio.backend/src/main/java/com/pagarme/desafio/backend/core/domain/Transaction.java
@@ -1,17 +1,29 @@
 package com.pagarme.desafio.backend.core.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.pagarme.desafio.backend.core.domain.enums.PaymentMethod;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class Transaction {
+
+    public Transaction(BigDecimal transactionPrice, String description, PaymentMethod paymentMethod, String cardNumber, String cardOwner, LocalDate expirationDate, short CVV) {
+        this.transactionPrice = transactionPrice;
+        this.description = description;
+        this.paymentMethod = paymentMethod;
+        this.cardNumber = this.convertCardNumber(cardNumber);
+        this.cardOwner = cardOwner;
+        this.expirationDate = expirationDate;
+        this.CVV = CVV;
+    }
 
     private BigDecimal transactionPrice;
 
@@ -23,14 +35,13 @@ public class Transaction {
 
     private String cardOwner;
 
-    private LocalDateTime expirationDate;
+    private LocalDate expirationDate;
 
     private short CVV;
 
-    public String ConvertCardNumber() {
-        return this.cardNumber.subSequence(this.cardNumber.length() - 4, this.cardNumber.length()).toString();
+    public String convertCardNumber(String cardNumber) {
+        return cardNumber.subSequence(cardNumber.length() - 4, cardNumber.length()).toString();
     }
-
     public boolean isCreditCard() {
         return this.paymentMethod == PaymentMethod.CREDIT_CARD;
     }
