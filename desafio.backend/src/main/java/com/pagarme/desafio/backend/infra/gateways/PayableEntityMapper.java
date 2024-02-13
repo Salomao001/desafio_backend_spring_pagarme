02b789848fc9f759f77cc57c42aa8321b91dd8df
Salomao <eduardo.salomao.584@gmail.com>
2024-02-12 22:22:11 -0300

    Adicionando como Component para Spring

--- desafio.backend/src/main/java/com/pagarme/desafio/backend/infra/gateways/PayableEntityMapper.java
+++ desafio.backend/src/main/java/com/pagarme/desafio/backend/infra/gateways/PayableEntityMapper.java
@@ -4,9 +4,11 @@ import com.pagarme.desafio.backend.core.domain.Payable;
 import com.pagarme.desafio.backend.core.domain.Transaction;
 import com.pagarme.desafio.backend.infra.entities.PayableEntity;
 import com.pagarme.desafio.backend.infra.entities.TransactionEntity;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+@Component
 public class PayableEntityMapper {
     public PayableEntity mapToEntity(Payable payable) {
         return new PayableEntity(payable);
--- desafio.backend/src/main/java/com/pagarme/desafio/backend/infra/gateways/TransactionEntityMapper.java
+++ desafio.backend/src/main/java/com/pagarme/desafio/backend/infra/gateways/TransactionEntityMapper.java
@@ -2,7 +2,9 @@ package com.pagarme.desafio.backend.infra.gateways;
 
 import com.pagarme.desafio.backend.core.domain.Transaction;
 import com.pagarme.desafio.backend.infra.entities.TransactionEntity;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TransactionEntityMapper {
     public TransactionEntity mapToEntity(Transaction transaction) {
         return new TransactionEntity(transaction);
